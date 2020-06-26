ae5d032c2c2ad10b2e45b430c58472decf4f65cf
Chris Gonzalez <cmgonza89@gmail.com>
2020-06-25 21:25:13 -0500

    initial commit

---
+++ db.sql
@@ -0,0 +1,18 @@
+-- for help \?
+
+-- list db's \l
+
+-- create database <database name>
+
+-- list all tables \d
+
+-- list all scheme \d <table name>
+
+-- products table
+
+CREATE TABLE products (
+  id INT,
+  name VARCHAR(50),
+  price INT,
+  on_sale boolean
+);
